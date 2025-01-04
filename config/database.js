3d38cfb0d14d6398b2ab968c0f4c28f13332d75e
ysmreg <33682244+ysmreg@users.noreply.github.com>
2025-01-04 13:22:16 +0900

    update : migrate可能にした

---
+++ config/database.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'p2p_ads_dev',
+    host: process.env.DB_HOST || '127.0.0.1',
+    dialect: 'mariadb',
+    logging: false
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mariadb',
+    logging: false
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mariadb',
+    logging: false
+  }
+};
